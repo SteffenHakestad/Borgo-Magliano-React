87eaebb1a7bf6251a70be946b045aec9782818bf
SteffenHakestad <Hakestad.steffen@gmail.com>
2023-12-21 16:53:09 +0100

    Work on responsiveness on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,6 +11,21 @@ export default function Navbar() {
             <CustomLink to="/chat" className="navbar-link">Chat</CustomLink>
             <CustomLink to="/member" className="navbar-link">Medlem</CustomLink>
         </ul>
+        <div id="toggle-button">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </div>
+        <div id="navbar-mobile-links">
+            <ul>
+                <CustomLink to="/news" className="navbar-link">Nyheter</CustomLink>
+                <CustomLink to="/events" className="navbar-link">Eventer</CustomLink>
+                <CustomLink to="/gallery" className="navbar-link">Galleri</CustomLink>
+                <CustomLink to="/blog" className="navbar-link">Blogg</CustomLink>
+                <CustomLink to="/chat" className="navbar-link">Chat</CustomLink>
+                <CustomLink to="/member" className="navbar-link">Medlem</CustomLink>
+            </ul>
+        </div>
     </nav>
 }
 
@@ -23,4 +38,27 @@ function CustomLink ({ to, children, ...props}) {
         </li>
     )
 }
+//Function to detect if the page is scrolled.
+window.onscroll = function() {scrollFunction()};
+
+let originalFontSize = 5.5;
+//If the page is scrolled, the navbar will shrink and the font size will decrease.
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    console.log("Page is not at the top")
+    document.getElementById("navbar").style.height = "5rem";
+    let newFontSize = originalFontSize * 0.7;
+    document.getElementById("navbar-title").style.fontSize = newFontSize + "rem";
+    document.getElementById("toggle-button").style.marginTop = "0rem";
+
+  } else {
+    console.log("Page is at the top")
+    document.getElementById("navbar").style.height = "7rem";
+    document.getElementById("navbar-title").style.fontSize = originalFontSize + "rem";
+    document.getElementById("toggle-button").style.marginTop = "1rem";
+
+
+  }
+} 
+// vvvv This is a link to dashboard page. Should not be accessible from the navbar, but is here for testing purposes
 //<CustomLink to="/dashboard" className="navbar-link">(temp)Dashboard</CustomLink>
